1f09763bfec1868ac1ca363f6f74d5efab11b09d
uutaein <uutaein@gmail.com>
2019-11-30 23:47:33 +0900

    Made by 201521005 박병건

--- Frontend/functions/test/index.js
+++ Frontend/functions/test/index.js
@@ -1,9 +1,23 @@
 const app = require('express')()
 
-app.get('/',(req,res) => {
-    res.send('read ok')
-});
+//Made by 201521005 , 박병건 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'db-mysql.cj5nhbnzm5ke.ap-northeast-2.rds.amazonaws.com',
+    user     : 'admin',
+    password : 'adminroot',
+    port     :3306,
+    database : 'stockdb'
+})
+connection.connect();
+//Made by 201521005 , 박병건 
+app.get('/',function(req,res,next){
+    connection.query(`SELECT * from c000020 WHERE date>20191120`,function(err,result){
+        console.log(result);
 
+        res.send(result);
+    })
+})
 
 
 module.exports = app

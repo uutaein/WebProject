5e7f5aa97704192932a8c28dc5b59b7b158aac64
ParkByungGeon <zzqudrjs@ajou.ac.kr>
2019-12-09 20:29:57 +0900

    추천사이트 라우팅

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -59,11 +59,11 @@ export default new Router({
             name : 'InvestmentGame',
             component:()=>import('../Layout/Components/InvestmentGame.vue'),
         },
-        // {
-        //     path: '/pages/Recommend',
-        //     name : 'Recommend',
-        //     component:()=>import('../Layout/Components/RecommendStrategy.vue'),
-        // }
+        {
+            path: '/pages/Recommend',
+            name : 'Recommend',
+            component:()=>import('../Layout/Components/RecommendStrategy.vue'),
+        }
         
     ]
 })
